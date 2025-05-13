0123bcd0e33614ba346a18383e5681fbf94cec5d
3str3ll4V3r <156131289+3str3ll4V3r@users.noreply.github.com>
2025-05-12 21:53:13 -0600

    Initial Commit

---
+++ LiverpoolHack/LiverpoolHackApp.swift
@@ -0,0 +1,32 @@
+//
+//  LiverpoolHackApp.swift
+//  LiverpoolHack
+//
+//  Created by Estrella Verdiguel on 12/05/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LiverpoolHackApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ LiverpoolHackTests/LiverpoolHackTests.swift
@@ -0,0 +1,16 @@
+//
+//  LiverpoolHackTests.swift
+//  LiverpoolHackTests
+//
+//  Created by Estrella Verdiguel on 12/05/25.
+//
+
+import Testing
+
+struct LiverpoolHackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
